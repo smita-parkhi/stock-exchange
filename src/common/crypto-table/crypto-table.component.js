039b056669fc02b715653624b47106deb14844e0
Smita Parkhi <smita.chande18@gmail.com>
2021-04-27 18:22:29 +0530

    Update crypto table styles

--- src/common/crypto-table/crypto-table.component.js
+++ src/common/crypto-table/crypto-table.component.js
@@ -24,8 +24,8 @@ export default function CryptoTable({isDarkMode}) {
                 </td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;42,96,000</td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;42,96,000 / &#8377;43,04,000</td>
-                <td className='table-data color-green' style={{color: isDarkMode ? 'white' : 'black'}}>0.43%</td>
-                <td className='table-data color-green' style={{color: isDarkMode ? 'white' : 'black'}}>&#9650; &#8377;18,418</td>
+                <td className='table-data color-green'>0.43%</td>
+                <td className='table-data color-green'>&#9650; &#8377;18,418</td>
             </tr>
             <tr className='table-row' style={{backgroundColor: isDarkMode ? '#2e3241' : '#f8f9fa'}}>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>2</td>
@@ -35,8 +35,8 @@ export default function CryptoTable({isDarkMode}) {
                 </td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;42,75,896</td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;42,75,896 / &#8377;42,87,342</td>
-                <td className='table-data color-red' style={{color: isDarkMode ? 'white' : 'black'}}>-0.04%</td>
-                <td className='table-data color-red' style={{color: isDarkMode ? 'white' : 'black'}}>&#9660; &#8377;1,685</td>
+                <td className='table-data color-red'>-0.04%</td>
+                <td className='table-data color-red'>&#9660; &#8377;1,685</td>
             </tr>
             <tr className='table-row' style={{backgroundColor: isDarkMode ? '#2e3241' : '#f8f9fa'}}>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>3</td>
@@ -46,8 +46,8 @@ export default function CryptoTable({isDarkMode}) {
                 </td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;42,00,063</td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;42,00,063 / &#8377;42,99,999</td>
-                <td className='table-data color-red' style={{color: isDarkMode ? 'white' : 'black'}}>-1.81%</td>
-                <td className='table-data color-red' style={{color: isDarkMode ? 'white' : 'black'}}>&#9660; &#8377;77,518</td>
+                <td className='table-data color-red'>-1.81%</td>
+                <td className='table-data color-red'>&#9660; &#8377;77,518</td>
             </tr>
             <tr className='table-row' 
             style={{backgroundColor: isDarkMode ? '#2e3241' : '#f8f9fa'}}>
@@ -58,8 +58,8 @@ export default function CryptoTable({isDarkMode}) {
                 </td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;42,72,532</td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;42,72,532 / &#8377;43,15,473</td>
-                <td className='table-data color-red' style={{color: isDarkMode ? 'white' : 'black'}}>-0.12%</td>
-                <td className='table-data color-red' style={{color: isDarkMode ? 'white' : 'black'}}>&#9660; &#8377;5,049</td>
+                <td className='table-data color-red'>-0.12%</td>
+                <td className='table-data color-red'>&#9660; &#8377;5,049</td>
             </tr>
             <tr className='table-row' style={{backgroundColor: isDarkMode ? '#2e3241' : '#f8f9fa'}}>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>5</td>
@@ -69,8 +69,8 @@ export default function CryptoTable({isDarkMode}) {
                 </td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;43,15,000</td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;43,15,000 / &#8377;43,10,000</td>
-                <td className='table-data color-green' style={{color: isDarkMode ? 'white' : 'black'}}>0.87%</td>
-                <td className='table-data color-green' style={{color: isDarkMode ? 'white' : 'black'}}>&#9650; &#8377;37,418</td>
+                <td className='table-data color-green'>0.87%</td>
+                <td className='table-data color-green'>&#9650; &#8377;37,418</td>
             </tr>
             <tr className='table-row'style={{backgroundColor: isDarkMode ? '#2e3241' : '#f8f9fa'}}>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>6</td>
@@ -80,8 +80,8 @@ export default function CryptoTable({isDarkMode}) {
                 </td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;43,06,000</td>
                 <td className='table-data' style={{color: isDarkMode ? 'white' : 'black'}}>&#8377;43,06,000 / &#8377;43,25,000</td>
-                <td className='table-data color-green' style={{color: isDarkMode ? 'white' : 'black'}}>0.66%</td>
-                <td className='table-data color-green' style={{color: isDarkMode ? 'white' : 'black'}}>&#9650; &#8377;28,418</td>
+                <td className='table-data color-green'>0.66%</td>
+                <td className='table-data color-green'>&#9650; &#8377;28,418</td>
             </tr>
         </tbody>
     </table>
